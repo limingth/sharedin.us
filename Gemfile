8a7be207959904236439b3fa7cadda3be6e20d05
Li Ming <limingth@gmail.com>
2014-01-04 17:42:13 -0800

    Add Devise for sign in/out

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'twitter-bootstrap-rails'
 gem 'less-rails'
 gem 'therubyracer'
 
+gem 'devise'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 SharedinUs::Application.routes.draw do
+  devise_for :users
   get "pub/dir"
   get "nhome/create"
   get "welcome/index"
 
+devise_scope :user do
+  get "sign_in", :to => "devise/sessions#new"
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+#  root 'welcome#index'
+  root to: "home#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
