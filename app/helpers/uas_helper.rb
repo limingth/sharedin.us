d21bb23c5d4db6e07c5b76ae551a39e3b90b9280
Li Ming <limingth@gmail.com>
2014-02-26 12:48:27 +0800

    Add Gravatar feature to search tag result

--- app/helpers/uas_helper.rb
+++ app/helpers/uas_helper.rb
@@ -1,2 +1,8 @@
 module UasHelper
+  def gravatar_for(user_id)
+    user = User.find(user_id)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=50"
+    image_tag(gravatar_url, alt: user.firstname, class: "gravatar")
+  end 
 end
